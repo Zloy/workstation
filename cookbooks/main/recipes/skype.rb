ed807c9cd5338a929ba2adc7d147bd6cb6885c78
Zloy <zhoran@inbox.ru>
2018-05-12 18:53:06 +0300

    Make skype recipe dependent on GUI presense

--- cookbooks/main/recipes/skype.rb
+++ cookbooks/main/recipes/skype.rb
@@ -1,6 +1,10 @@
-apt_repository 'canonical_partner' do
-  uri 'http://archive.canonical.com/ubuntu/'
-  components ['saucy', 'partner']
-end
+extend Main::Helper
 
-package 'skype'
+if has_gui?
+  apt_repository 'canonical_partner' do
+    uri 'http://archive.canonical.com/ubuntu/'
+    components ['trusty', 'partner']
+  end
+
+  package 'skype'
+end
