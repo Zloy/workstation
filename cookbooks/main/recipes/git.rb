7463ba0560ed3aed0d59cf90945445cf6bfc9a5a
Zloy <zhoran@inbox.ru>
2018-05-15 11:07:56 +0300

    Rewrite git recipe

--- cookbooks/main/recipes/git.rb
+++ cookbooks/main/recipes/git.rb
@@ -6,6 +6,7 @@ file "/home/#{node[:user]}/.cvsignore" do
   not_if node[:git][:cvsignore_entries].empty?
 end
 
+=begin
 node[:git][:settings].each do |setting, value|
   execute "git #{setting}" do
     user node[:user]
@@ -13,3 +14,13 @@ node[:git][:settings].each do |setting, value|
     command "git config --file /home/#{node[:user]}/.gitconfig #{setting} #{value}"
   end
 end
+=end
+
+template "/home/#{node[:user]}/.gitconfig" do
+  source 'gitconfig'
+  user node[:user]
+  group node[:user]
+  variables(github_user: node[:github_user],
+            github_email: node[:github_email])
+end
+
