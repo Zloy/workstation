14eabf55a66b6a8390971d2dfd89725ae6e7889b
Zloy <zhoran@inbox.ru>
2018-05-16 09:37:42 +0300

    Fix disable_screen_lock recipe

--- cookbooks/main/recipes/disable_screen_lock.rb
+++ cookbooks/main/recipes/disable_screen_lock.rb
@@ -1,5 +1,6 @@
 execute 'Disable screen lock' do
   user ENV['SUDO_USER']
-  command 'dconf write /org/gnome/desktop/screensaver/lock-enabled false'
+  command 'dconf write /org/gnome/desktop/screensaver/lock-enabled false &&
+           gsettings set org.gnome.desktop.session idle-delay 0'
 end
 
