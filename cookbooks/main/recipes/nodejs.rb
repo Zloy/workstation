b98925f89729a6a762672d48900fa5efd43d367f
Zloy <zhoran@inbox.ru>
2018-08-01 12:07:03 +0300

    Update nodejs recipe

--- cookbooks/main/recipes/nodejs.rb
+++ cookbooks/main/recipes/nodejs.rb
@@ -1,7 +1,12 @@
-execute 'Install nodejs' do
+execute 'Install nodejs repo' do
   command 'curl -sL https://deb.nodesource.com/setup_10.x | bash -'
   user 'root'
 end
 
 apt_package %w(nodejs build-essential)
+
+execute 'Upgrade npm' do
+  command 'npm i -g npm@6.2.0'
+  user 'root'
+end
 
